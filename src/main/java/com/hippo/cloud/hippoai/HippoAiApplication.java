d029648fd793f3d796192b81b1d8a895941c8a3c
zjj <zhangjunjun@oigcn.com>
2025-05-09 16:18:49 +0800

    Initial commit

---
+++ src/main/java/com/hippo/cloud/hippoai/HippoAiApplication.java
@@ -0,0 +1,13 @@
+package com.hippo.cloud.hippoai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HippoAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HippoAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hippo/cloud/hippoai/HippoAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hippo.cloud.hippoai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HippoAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
